c44e1cebdd5ffa558a023a849c56ccd95befbdbb
Gustavo Caminha <gustavocsj13@gmail.com>
2022-11-15 15:31:35 -0300

    bottavinho

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import discord
+import os
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
 
 bot = commands.Bot("!")
 
@@ -12,4 +16,4 @@ async def ping(ctx):
     await ctx.send('Pong! {0}'.format(round(bot.latency, 1)))
 
 
-bot.run("MTA0MTAxODk5MTYwMDIxMDA1Mg.GOOtMx.xAu1MI7HQPJ5FNya-CkGxWNzjLU9M_8XlNP0xM")
+bot.run(os.getenv("TOKEN"))
