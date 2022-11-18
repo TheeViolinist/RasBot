515f04c2834b71c471cc0c824e68f78717540221
Leandro Lucas <leoViolinist22@gmail.com>
2022-11-18 15:44:50 -0300

    atualizando codigo

--- bot.py
+++ bot.py
@@ -1,19 +1,28 @@
 import discord
 import os
-from discord.ext import commands
+from discord.ext import commands # Importa a classe comands do pacote discord.ext
 from dotenv import load_dotenv
 
 load_dotenv()
 
-bot = commands.Bot("!")
 
-@bot.event
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
 async def on_ready():
-    print(f"Olá, estou conectado como {bot.user}")
+    print('We have logged in as{0.user}'.format(client))
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('Pong! {0}'.format(round(bot.latency, 1)))
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$Hello"):
+        await message.channel.send('Hello!')
 
 
-bot.run(os.getenv("TOKEN"))
+client.run(os.getenv("TOKEN"))
+
+
