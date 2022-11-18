b0197f2a57e01abfb66357f39d497d8277e08a75
Gustavo Caminha <gustavocsj13@gmail.com>
2022-11-18 16:39:50 -0300

    att1

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+import random
 import discord
 import os
 from discord.ext import commands # Importa a classe comands do pacote discord.ext
@@ -11,6 +10,7 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='!', intents=intents)
 
 @client.event
 async def on_ready():
@@ -24,8 +24,12 @@ async def on_message(message):
     if message.content.startswith("$Hello"):
         await message.channel.send('Hello!')
 
+@client.command()
+async def rolll(ctx, max):
+    number = random.randint(1,max)
+    await ctx.send(number)
+
 
 client.run(os.getenv("TOKEN"))
 
 
->>>>>>> 515f04c2834b71c471cc0c824e68f78717540221
